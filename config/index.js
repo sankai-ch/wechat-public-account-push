05e435c9df78a633179a925a6d6bd40401ba803e
山海 <30490337+sankai-ch@users.noreply.github.com>
2022-09-13 21:26:27 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -25,7 +25,7 @@ export const config = {
       // 想要发送的人的名字
       name: "老婆0",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "oXalv6Qe1JL4VY6PijAYsRxgdhYw",
+      id: "oXalv6Xjr8_GNin-K88zafkmCRdM",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "178MV52k0j0aJkgktLiw8HQhiKWinNNXDhG5GS0bsSI",
       // 所在省份
@@ -33,53 +33,31 @@ export const config = {
       // 所在城市
       city: "杭州",
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '12-27',
+      horoscopeDate: '11-05',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://wangxinleo.cn",
-      // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09"},
-        {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
-        {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
-      ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         // 在一起的日子
-        {"keyword": "love_day", date: "2022-09-08"},
-        // 结婚纪念日
-        {"keyword": "marry_day", date: "2022-09-09"},
-        // 退伍日
-        {"keyword": "ex_day", date: "2022-09-10"},
+        {"keyword": "love_day", date: "2022-02-28"},
       ]
     },
     {
-      name: "老婆1",
-      id: "oXalv6Xjr8_GNin-K88zafkmCRdM",
+      name: "自己",
+      id: "oXalv6Qe1JL4VY6PijAYsRxgdhYw",
       useTemplateId: "178MV52k0j0aJkgktLiw8HQhiKWinNNXDhG5GS0bsSI",
       province: "浙江",
       city: "杭州",
-      horoscopeDate: '',
-      horoscopeDateType: '',
+      horoscopeDate: '08-13',
+      horoscopeDateType: '今日',
       openUrl: "https://wangxinleo.cn",
       festivals: [],
-      customizedDateList: []
-    },
-    {
-      name: "老婆2",
-      id: "",
-      useTemplateId: "",
-      province: "",
-      city: "",
-      horoscopeDate: '',
-      horoscopeDateType: '',
-      openUrl: "https://wangxinleo.cn",
-      festivals: [],
-      customizedDateList: []
+      customizedDateList: [
+      // 在一起的日子
+        {"keyword": "love_day", date: "2022-02-28"},
+      ]
     }
     // 你可以不断按格式往下增加
     // ...
@@ -136,11 +114,13 @@ export const config = {
    */
   FESTIVALS: [
     // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-    {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09", isShowAge: true},
-    {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
     // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-    {"type": "生日", "name": "李四", "year": "1996", "date": "09-31", isShowAge: true},
-    {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+    {"type": "生日", "name": "老婆", "year": "1997", "date": "11-05", isShowAge: true},
+    {"type": "节日", "name": "春节", "year": "2023", "date": "01-22"},
+    {"type": "节日", "name": "元旦", "year": "2023", "date": "01-01"},
+    {"type": "节日", "name": "国庆", "year": "2022", "date": "10-01"},
+    {"type": "节日", "name": "平安夜", "year": "2022", "date": "12-24"},
+    {"type": "节日", "name": "圣诞节", "year": "2022", "date": "12-25"},
     // 你可以不断按格式往下增加
     // ...
   ],
@@ -150,7 +130,7 @@ export const config = {
    * 如果为3, 则仅展示“将要到达” 的3个重要节日提醒，剩下的将被忽略
    * 如果为0, 则默认展示全部
    */
-  FESTIVALS_LIMIT: 4,
+  FESTIVALS_LIMIT: 3,
 
   /** 日期相关 */
 
@@ -160,7 +140,7 @@ export const config = {
     * */
   CUSTOMIZED_DATE_LIST: [
     // 在一起的日子
-    {"keyword": "love_day", date: "2022-09-08"},
+    {"keyword": "love_day", date: "2022-02-28"},
     // 结婚纪念日
     {"keyword": "marry_day", date: "2022-09-09"},
     // 退伍日
