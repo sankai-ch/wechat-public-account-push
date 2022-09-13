c4d240c5641db36a2c2cd6871b3a7a61b14142a6
山海 <30490337+sankai-ch@users.noreply.github.com>
2022-09-13 21:02:13 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -25,13 +25,13 @@ export const config = {
       // 想要发送的人的名字
       name: "老婆0",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "oXalv6Qe1JL4VY6PijAYsRxgdhYw",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "",
+      useTemplateId: "178MV52k0j0aJkgktLiw8HQhiKWinNNXDhG5GS0bsSI",
       // 所在省份
-      province: "广东",
+      province: "浙江",
       // 所在城市
-      city: "肇庆",
+      city: "杭州",
       // 新历生日, 仅用作获取星座运势, 格式必须
       horoscopeDate: '12-27',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
